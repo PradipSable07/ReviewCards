9a805cbb3e0ac85fe071f0b2fcb7adafe6f7843c
Pradip Sable <sablepradipsubhash@gmail.com>
2023-10-22 11:01:04 +0530

    added random btn and changed the slidechage code

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,34 @@ import { BiSolidQuoteRight } from "react-icons/bi";
 function App() {
 	const [index, setIndex] = useState(0);
 	const [animation, setAnimation] = useState("");
-	people[index];
+
 	const nextSlide = () => {
 		setTimeout(() => {
 			setAnimation("animate-LeftWobble");
-			setIndex(index === people.length - 1 ? 0 : index + 1);
+			setIndex((i) => {
+				const newI = (i + 1) % people.length;
+				return newI;
+			});
 		});
 		setAnimation("");
 	};
 	const prevSlide = () => {
 		setTimeout(() => {
 			setAnimation("animate-RightWobble");
-			setIndex(index === 0 ? people.length - 1 : index - 1);
+			setIndex((i) => {
+				const newI = (i - 1 + people.length) % people.length;
+				return newI;
+			});
 		});
 		setAnimation("");
+	};
+	const randomReview = () => {
+		let randomNumber = Math.floor(Math.random() * people.length);
+		if (randomNumber === index) {
+			randomNumber = index + 1;
+		}
+		const newI = randomNumber % people.length;
+		setIndex(newI);
 	};
 
 	const { name, image, job, text } = people[index];
@@ -50,6 +64,17 @@ function App() {
 							className=' bg-transparent mx-4 text-2xl font-semibold border-transparent border-[#A7D397] hover:bg-[#A7D397] hover:text-white py-2 px-2 rounded-full hover:shadow-xl hover:animate-wobble '>
 							<BsArrowLeft />
 						</button>
+						<a
+							onClick={randomReview}
+							href='#_'
+							class='relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group'>
+							<span class='absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease'></span>
+							<span class='absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease'>
+								<span class='absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md'></span>
+								<span class='absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md'></span>
+							</span>
+							<span class='relative text-white'>Random Review</span>
+						</a>
 						<button
 							onClick={nextSlide}
 							className=' bg-transparent text-2xl font-semibold border-transparent border-[#A7D397] hover:bg-[#A7D397] hover:text-white py-2 px-2 rounded-full hover:shadow-xl hover:animate-wobble'>
